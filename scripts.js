a8b87de3715a195042d7327a565402a1a3e92f08
ForkEyeee <shawncarter123456@gmail.com>
2023-06-09 08:03:29 -0400

    added node class and buildtree method

--- scripts.js
+++ scripts.js
@@ -1,19 +1,40 @@
-class GameBoard {
-  constructor(knight) {
-    this.gameBoard = this.generateGameBoard()
-    this.knight = knight
-  }
-  generateGameBoard(x = 0, arr = []) {
-    if (x >= 9) {
-      return arr
-    } else {
-      for (let i = 0; i <= 8; i++) {
-        arr.push([x, i])
-        console.log(arr)
-      }
-      return this.generateGameBoard(++x, arr)
-    }
+class Node {
+  constructor(data = null, left = null, right = null) {
+    this.data = data
+    this.left = left
+    this.right = right
   }
 }
 
-const test = new GameBoard()
+class GameBoard {
+  constructor(knight) {
+    this.gameBoard = this.generateGameBoard()
+    this.tree = this.buildTree(this.gameBoard)
+    this.knight = knight
+  }
+
+  generateGameBoard(j = 0, arr = [[], [], [], [], [], [], [], [], []]) {
+    if (j >= 9) {
+      return arr
+    } else {
+      for (let i = 0; i <= 8; i++) {
+        arr[j].push([j, i])
+      }
+      return this.generateGameBoard(++j, arr)
+    }
+  }
+
+  buildTree(arr, start = 0, end = arr.length - 1) {
+    if (start > end) {
+      return null
+    }
+    let mid = Math.floor((start + end) / 2)
+    const node = new Node(arr[mid])
+    node.left = this.buildTree(arr, start, mid - 1)
+    node.right = this.buildTree(arr, mid + 1, end)
+    return node
+  }
+  knightMoves(x, y) {}
+}
+
+const test = new GameBoard([3,4])
